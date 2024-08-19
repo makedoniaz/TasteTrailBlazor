c9cad2ac69bb9361ad3aac17fa9da1840383cb84
Zeynab Mahammadova <zeynabmahammadov@outlook.com>
2024-08-20 01:27:12 +0400

    Dashboard edit

--- TasteTrail/src/Models/User.cs
+++ TasteTrail/src/Models/User.cs
@@ -5,21 +5,19 @@ using System.Threading.Tasks;
 
 namespace TasteTrailBlazor.Models;
 
-public class User  
+public class User
 {
     public bool IsBanned { get; set; }
-
     public bool IsMuted { get; set; }
-
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string? PhoneNumber { get; set; }
     public string? AvatarPath { get; set; }
 
     public ICollection<Feedback> Feedbacks { get; set; }
-
     public ICollection<Venue> Venues { get; set; }
-
     public ICollection<AccessToken> RefreshTokens { get; set; }
-
     public ICollection<MenuItem> MenuItems { get; set; }
-
     public ICollection<Menu> Menus { get; set; }
 }
---
+++ TasteTrail/src/Models/UserRoles.cs
@@ -0,0 +1,9 @@
+namespace TasteTrailBlazor.Models;
+
+public enum UserRoles
+{
+    Admin = 1,
+    User = 2,
+    Moderator = 3
+    // Add other roles as needed
+}
